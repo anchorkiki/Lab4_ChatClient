a3c737279b5e63f34689ed5cd9f7c0689d0ae021
蔡俊杰 <16532289+junjiecai@user.noreply.gitee.com>
2026-01-02 17:24:47 +0800

    客户端UI美化

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include "connectwindow.h"
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // 加载样式表
+    QFile styleFile(":/style.qss");  // 如果放在资源文件中
+
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QString styleSheet = QLatin1String(styleFile.readAll());
+        a.setStyleSheet(styleSheet);
+        styleFile.close();
+    } else {
+        qDebug() << "无法加载样式表文件";
+    }
+
     ConnectWindow w;
     w.show();
     return a.exec();
